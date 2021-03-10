4d55051ef1b6b47b8f152789a9667fbbe3cfb472
olbo81 <olbo81@mail.ru>
2021-03-10 15:34:32 +0500

    Initial commit

---
+++ src/test/java/com/boleg/springboot/front/jquery/JqueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boleg.springboot.front.jquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JqueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
